a5cc4cc12b3f2213fe0a7fdd20165ab3e5b1679b
xy1xy <2442596+xy1xy@users.noreply.github.com>
2021-07-12 21:17:36 +1000

    Init commit: boilerplate things

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  plugins: ['jest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+  ],
+  parserOptions: { project: './tsconfig.json' },
+  env: { 'jest/globals': true },
+  ignorePatterns: ['node_modules', 'dist', 'coverage'],
+  rules: {
+    // TS
+    '@typescript-eslint/no-explicit-any': [
+      'error', { 'fixToUnknown': true, 'ignoreRestArgs': true },
+    ],
+    // ES:off + TS:on
+    'semi': 'off',
+    '@typescript-eslint/semi': ['error', 'never'],
+    'indent': 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    'quotes': 'off',
+    '@typescript-eslint/quotes': [
+      'error', 'single', { 'allowTemplateLiterals': true },
+    ],
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+    // ES
+    'max-len': ['error', { 'code': 80 }],
+    'eol-last': ['error', 'always'],
+    'no-tabs': ['error', { 'allowIndentationTabs': false }],
+    'comma-spacing': ['error', { 'before': false, 'after': true }],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'no-multi-spaces': 'error',
+    'space-in-parens': ['error', 'never'],
+    'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }],
+    'func-call-spacing': ['error', 'never'],
+  },
+}

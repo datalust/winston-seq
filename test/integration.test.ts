34add1335cf698657ab04d633b8a0b978bc48dd1
Liam McLennan <liam.mclennan@gmail.com>
2021-10-29 14:17:31 +1100

    moved type definition

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -2,34 +2,45 @@
 import winston from 'winston'
 import winstonSeq from '../src/index'
 
+let logger: winston.Logger, transport: winstonSeq
+
 describe('@integration', () => {
-  it('should log to the docker Seq server', async () => {
-    const seq = new winstonSeq({
-      onError: console.error,
+  beforeAll(() => {
+    console.log(`
+    ****************
+    
+    Needs a seq server
+    
+    ***************`)
+
+
+    transport = new winstonSeq({
+      serverUrl: '*******',
+      apiKey: '********',
+      onError: (e => { console.error(e) }),
+      handleExceptions: true,
+      handleRejections: true,
     })
-    const logger = winston.createLogger({
+    logger = winston.createLogger({
       level: 'debug',
-      defaultMeta: { service: 'test-service' },
-      transports: [seq],
-    })
-
-    const child = logger.child({ someContext: [1, 2, 3] })
-    child.debug('debug {service} {someContext}')
-    child.info('info {service} {someContext}')
-    child.warn('warn {service} {someContext}')
-    child.error('error {service} {someContext}')
-    child.error('error with Error', new Error('some error'))
-    child.error(new Error('Error'))
-
-    return new Promise(resolve => {
-      seq.on('flushed', () => {
-        console.log('Received seq:flushed')
-      })
-      seq.on('closed', () => {
-        console.log('Received seq:closed')
-        resolve(null)
-      })
-      logger.close()
+      defaultMeta: { application: 'logtests' },
+      transports: [
+        transport,
+      ],
     })
   })
+
+  afterAll(async () => {
+    return new Promise(resolve => {
+      logger.close()
+      resolve(null)
+    })
+  })
+
+
+  it('should send a log to seq', async () => {
+    const childLogger = logger.child({ requestId: '451' })
+    childLogger.info('Logging from child for the {n}th time, {user}!', { n: 7, user: 'Bob' })
+  })
+
 })
